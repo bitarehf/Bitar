aea93c5fc82edd62943b8e0590f877f86444a331
Saevar2000 <saevar@bitar.is>
2018-10-21 06:21:14 +0000

    Disable kestrel https, using nginx for that

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace Bitar
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://localhost:5000")
                 .UseStartup<Startup>();
     }
 }
