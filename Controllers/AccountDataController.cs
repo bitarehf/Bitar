6a93820f0474171ffa5c9c5aa7d84cc8de0630fc
Saevar2000 <saevar@bitar.is>
2019-02-03 17:18:11 +0000

    Refactor PersonsController to AccountDataController

--- Controllers/AccountDataController.cs
+++ Controllers/AccountDataController.cs
@@ -10,42 +10,42 @@ namespace Bitar.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    public class PersonsController : ControllerBase
+    public class AccountDataDetailsController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
 
-        public PersonsController(ApplicationDbContext context)
+        public AccountDataDetailsController(ApplicationDbContext context)
         {
             _context = context;
         }
 
-        // GET: api/Persons
+        // GET: api/AccountData
         [HttpGet]
-        public List<AccountData> GetPersons()
+        public List<AccountData> AllAccountData()
         {
             return _context.AccountData.ToList();
         }
 
-        // GET: api/Persons/5
+        // GET: api/AccountData/4708180420
         [HttpGet("{id}")]
-        public async Task<ActionResult<AccountData>> GetPerson(string id)
+        public async Task<ActionResult<AccountData>> AccountData(string id)
         {
             return await _context.AccountData.FindAsync(id);
         }
 
-        // POST: api/Persons
+        // POST: api/AccountData
         /// <summary>
-        /// Updates Persons WithdrawalAddress
+        /// Updates Account WithdrawalAddress
         /// </summary>
         [HttpPost]
         public async Task<ActionResult<BitcoinAddress>> UpdateWithdrawalAddress(string id, BitcoinAddress bitcoinAddress)
         {
-            var person = await _context.AccountData.FindAsync(id);
-            person.WithdrawalAddress = bitcoinAddress.ToString();
+            var accountData = await _context.AccountData.FindAsync(id);
+            accountData.WithdrawalAddress = bitcoinAddress.ToString();
 
             await _context.SaveChangesAsync();
 
-            return BitcoinAddress.Create(person.WithdrawalAddress, Network.Main);
+            return BitcoinAddress.Create(accountData.WithdrawalAddress, Network.Main);
         }
     }
 }
