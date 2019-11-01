65d0c1e4226d1e5577b11bc23816bf3f751a638a
Saevar2000 <saevar@bitar.is>
2019-11-01 23:02:07 +0000

    Handle concurrency exceptions

--- Repositories/MarketRepository.cs
+++ Repositories/MarketRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Bitar.Models;
 using Bitar.Services;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NBitcoin;
@@ -26,81 +27,90 @@ namespace Bitar.Repositories
         }
         public async Task<uint256> Order(string id, decimal isk)
         {
-            var accountData = await _context.AccountData.FindAsync(id);
-            if (accountData == null)
+            try
             {
-                _logger.LogCritical($"Order cancelled because no account with id: {id} was found");
-                return null;
-            }
-
-            decimal rate = Decimal.Zero;
-
-            using (var scope = _serviceProvider.CreateScope())
-            {
-                var _stockService = scope.ServiceProvider.GetRequiredService<StockService>();
-
-                if (_stockService.MarketState == MarketState.Open)
+                var accountData = await _context.AccountData.FindAsync(id);
+                if (accountData == null)
                 {
-                    foreach (var stock in _stockService.Stocks)
+                    _logger.LogCritical($"Order cancelled because no account with id: {id} was found");
+                    return null;
+                }
+
+                decimal rate = Decimal.Zero;
+
+                using (var scope = _serviceProvider.CreateScope())
+                {
+                    var _stockService = scope.ServiceProvider.GetRequiredService<StockService>();
+
+                    if (_stockService.MarketState == MarketState.Open)
                     {
-                        if (stock.Symbol == Symbol.BTC)
+                        foreach (var stock in _stockService.Stocks)
                         {
-                            rate = stock.Price;
+                            if (stock.Symbol == Symbol.BTC)
+                            {
+                                rate = stock.Price;
+                            }
                         }
+                    }
+                    else
+                    {
+                        _logger.LogCritical("Order cancelled because market is closed.");
+                        return null;
+                    }
+                }
+
+                if (rate == Decimal.Zero)
+                {
+                    _logger.LogCritical("Order cancelled because rate value was zero, this should never happen.");
+                    return null;
+                }
+
+                Money coins = Money.Coins(Math.Round(isk / rate, 8, MidpointRounding.ToZero));
+                _logger.LogDebug($"Id: {id} Coins: {coins} ISK: {isk} Rate: {rate} Account Balance: {accountData.Balance}");
+                if (accountData.Balance >= isk)
+                {
+                    // var transaction = new Bitar.Models.MarketTransaction
+                    // {
+                    //     PersonalId = id,
+                    //     Date = DateTime.Now,
+                    //     Rate = rate,
+                    //     Coins = coins,
+                    //     Amount = isk,
+                    // };
+
+                    _logger.LogDebug($"{id} has sufficient balance for the order");
+
+                    accountData.Balance -= isk;
+                    // if (accountData.Balance < Decimal.Zero)
+                    // {
+                    //     _logger.LogCritical($"Balance can't be negative. Id: {id}");
+                    //     return null;
+                    // }
+                    await _context.SaveChangesAsync();
+
+                    _logger.LogWarning($"{id} bought {coins} Bitcoin for {isk} ISK at the rate of {rate}");
+                    using (var scope = _serviceProvider.CreateScope())
+                    {
+                        var _bitcoin = scope.ServiceProvider.GetRequiredService<BitcoinService>();
+                        return await _bitcoin.MakePayment(id, coins);
                     }
                 }
                 else
                 {
-                    _logger.LogCritical("Order cancelled because market is closed.");
+                    _logger.LogCritical(
+                        "Order cancelled.\n" +
+                        $"{id} does not have sufficient balance for the order.\n" +
+                        $"Order => {coins} BTC for {isk} ISK.\n" +
+                        $"Current balance: {accountData.Balance} ISK.");
                     return null;
                 }
             }
-
-            if (rate == Decimal.Zero)
+            catch (DbUpdateConcurrencyException)
             {
-                _logger.LogCritical("Order cancelled because rate value was zero, this should never happen.");
-                return null;
+                _logger.LogError($"DbUpdateConcurrencyException: {isk} ISK order cancelled.");
             }
 
-            Money coins = Money.Coins(Math.Round(isk / rate, 8, MidpointRounding.ToZero));
-            _logger.LogDebug($"Id: {id} Coins: {coins} ISK: {isk} Rate: {rate} Account Balance: {accountData.Balance}");
-            if (accountData.Balance >= isk)
-            {
-                // var transaction = new Bitar.Models.MarketTransaction
-                // {
-                //     PersonalId = id,
-                //     Date = DateTime.Now,
-                //     Rate = rate,
-                //     Coins = coins,
-                //     Amount = isk,
-                // };
-
-                _logger.LogDebug($"{id} has sufficient balance for the order");
-                
-                accountData.Balance -= isk;
-                // if (accountData.Balance < Decimal.Zero)
-                // {
-                //     _logger.LogCritical($"Balance can't be negative. Id: {id}");
-                //     return null;
-                // }
-                await _context.SaveChangesAsync();
-
-                _logger.LogWarning($"{id} bought {coins} Bitcoin for {isk} ISK at the rate of {rate}");
-                using (var scope = _serviceProvider.CreateScope())
-                {
-                    var _bitcoin = scope.ServiceProvider.GetRequiredService<BitcoinService>();
-                    return await _bitcoin.MakePayment(id, coins);
-                }
-            }
-            else
-            {
-                _logger.LogCritical(
-                    "Order cancelled.\n" +
-                    $"{id} does not have sufficient balance for the order.\n" +
-                    $"Order => {coins} BTC for {isk} ISK.\n" +
-                    $"Current balance: {accountData.Balance} ISK.");
-                return null;
-            }
+            return null;
         }
     }
 }
