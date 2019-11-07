f48fb5201e22862cff98b0ddc4a5dc4229d7e117
Saevar2000 <saevar@bitar.is>
2019-11-07 23:10:41 +0000

    Add LastUpdated to Stock

--- Services/StockService.cs
+++ Services/StockService.cs
@@ -21,8 +21,9 @@ namespace Bitar.Services
         private Timer _timer;
         private decimal BTC { get; set; }
         private decimal ISK { get; set; }
-        public List<Stock> Stocks { get; set; }
-        public MarketState MarketState { get; set; }
+        public List<Stock> Stocks { get; private set; }
+        public MarketState MarketState { get; private set; }
+        public DateTime LastUpdated { get; private set; }
 
         public StockService(
             ILogger<StockService> logger,
@@ -56,7 +57,6 @@ namespace Bitar.Services
 
         public async void UpdateStockPrices(object state)
         {
-            List<Stock> stocks = new List<Stock>();
             List<Stock> landsbankinnStocks = await _landsbankinn.FetchCurrencyUpdates();
 
             decimal eurisk = decimal.Zero;
@@ -91,15 +91,29 @@ namespace Bitar.Services
 
             decimal btcisk = eurisk * btceur;
 
-            stocks.Add(new Stock() { Symbol = Symbol.BTC, Price = decimal.Ceiling(btcisk * 1.02m + 100m) });
-            stocks.Add(new Stock() { Symbol = Symbol.ISK, Price = 1m });
-            stocks.Add(new Stock() { Symbol = Symbol.EUR, Price = eurisk });
-            stocks.Add(new Stock() { Symbol = Symbol.USD, Price = usdisk });
+            LastUpdated = DateTime.Now;
+            var stocks = new List<Stock>
+            {
+                // We only include LastUpdated in the first stock
+                // since all the stocks are updated at the same time.
+                // This may or may not change.
+                new Stock {
+                    Symbol = Symbol.BTC, 
+                    Price = decimal.Ceiling(btcisk * 1.02m + 100m),
+                    LastUpdated = LastUpdated
+                    },
+                new Stock { Symbol = Symbol.ISK, Price = 1m },
+                new Stock { Symbol = Symbol.EUR, Price = eurisk },
+                new Stock { Symbol = Symbol.USD, Price = usdisk}
+            };
+
             Stocks = stocks;
             OpenMarket();
 
             // Send update to all SignalR Clients.
             await _hubContext.Clients.All.SendAsync("StocksUpdated", Stocks);
+
+            _logger.LogInformation($"Stocks Updated: {LastUpdated}");
         }
 
         public void OpenMarket()
@@ -110,7 +124,7 @@ namespace Bitar.Services
                 MarketState = MarketState.Open;
             }
         }
-        
+
         public void CloseMarket()
         {
             if (MarketState != MarketState.Closed)
@@ -131,6 +145,7 @@ namespace Bitar.Services
     {
         public decimal Price { get; set; }
         public Symbol Symbol { get; set; }
+        public DateTime? LastUpdated { get; set; }
     }
 
     public enum Symbol
