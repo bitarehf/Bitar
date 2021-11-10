4b72d71e948dbdf739a6a0a776fc37409539d410
Saevar2000 <saevar@bitar.is>
2021-11-10 01:08:41 +0000

    Upgrade from .net 5.0 to 6.0

--- Program.cs
+++ Program.cs
@@ -1,31 +1,122 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
+﻿using System.Text;
+using Bitar.Hubs;
+using Bitar.Models;
+using Bitar.Models.Settings;
+using Bitar.Repositories;
+using Bitar.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
-namespace Bitar
-{
-    public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddUserSecrets<Program>(optional: false); // Forces user secrets to load.
+
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((hostingContext, config) =>
-                {
-                    config.AddUserSecrets<Startup>(optional: false);
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseKestrel(serverOptions =>
-                    {
-                        // Set properties and call methods on options.
-                    });
-                    webBuilder.UseUrls("http://localhost:5000");
-                    webBuilder.UseStartup<Startup>();
-                });
-                
+        options.EnableSensitiveDataLogging();
+        options.UseNpgsql(
+            builder.Configuration.GetConnectionString("DefaultConnection"));
     }
+);
+
+builder.Services.AddDefaultIdentity<ApplicationUser>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+
+    // Lockout settings.
+    options.Lockout.MaxFailedAccessAttempts = 10;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters = "aábcdðeéfghiíjklmnoópqrstuvwxyýzþæöAÁBCDÐEÉFGHIÍJKLMNOÓPQRSTUVWXYÝZÞÆÖ0123456789 -._@+";
+    options.User.RequireUniqueEmail = true;
+});
+
+builder.Services
+    .AddAuthentication(options =>
+    {
+        // Identity made Cookie authentication the default.
+        // However, we want JWT Bearer Auth to be the default.
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+    })
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "https://bitar.is",
+            ValidAudience = "https://bitar.is",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("JwtSettings:JwtKey").Value)),
+        };
+    });
+
+builder.Services.Configure<ForwardedHeadersOptions>(options => options.ForwardedHeaders = ForwardedHeaders.All);
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
+builder.Services.Configure<BitcoinSettings>(builder.Configuration.GetSection("BitcoinSettings"));
+builder.Services.Configure<LandsbankinnSettings>(builder.Configuration.GetSection("LandsbankinnSettings"));
+builder.Services.Configure<KrakenSettings>(builder.Configuration.GetSection("KrakenSettings"));
+builder.Services.Configure<JaSettings>(builder.Configuration.GetSection("JaSettings"));
+builder.Services.Configure<DilisenseSettings>(builder.Configuration.GetSection("DilisenseSettings"));
+
+builder.Services.AddHttpClient<ArionService>();
+builder.Services.AddHttpClient<BlockchainService>();
+
+builder.Services.AddScoped<MarketRepository>();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddSingleton<BitcoinService>();
+builder.Services.AddSingleton<LandsbankinnService>();
+builder.Services.AddSingleton<AssetService>();
+builder.Services.AddSingleton<KrakenService>();
+builder.Services.AddSingleton<TickerService>();
+builder.Services.AddSingleton<OhlcService>();
+builder.Services.AddHostedService<MarketService>();
+
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddSignalR();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+
 }
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
+app.UseRouting();
+
+app.UseForwardedHeaders();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapHub<TickerHub>("/tickers");
+
+app.Run("http://localhost:5000");
--- Startup.cs
+++ Startup.cs
@@ -1,175 +1,174 @@
-﻿using System;
-using System.Text;
-using Bitar.Hubs;
-using Bitar.Models;
-using Bitar.Models.Settings;
-using Bitar.Repositories;
-using Bitar.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpOverrides;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
-using NSwag;
-using NSwag.Generation.Processors.Security;
+﻿// using System;
+// using System.Text;
+// using Bitar.Hubs;
+// using Bitar.Models;
+// using Bitar.Models.Settings;
+// using Bitar.Repositories;
+// using Bitar.Services;
+// using Microsoft.AspNetCore.Authentication.JwtBearer;
+// using Microsoft.AspNetCore.Builder;
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.AspNetCore.Http;
+// using Microsoft.AspNetCore.HttpOverrides;
+// using Microsoft.AspNetCore.Identity;
+// using Microsoft.EntityFrameworkCore;
+// using Microsoft.Extensions.Configuration;
+// using Microsoft.Extensions.DependencyInjection;
+// using Microsoft.Extensions.Hosting;
+// using Microsoft.IdentityModel.Tokens;
 
-namespace Bitar
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
 
-        public IConfiguration Configuration { get; }
+// namespace Bitar
+// {
+//     public class Startup
+//     {
+//         public Startup(IConfiguration configuration)
+//         {
+//             Configuration = configuration;
+//         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                {
-                    options.EnableSensitiveDataLogging();
-                    options.UseNpgsql(
-                        Configuration.GetConnectionString("DefaultConnection"));
-                }
-            );
+//         public IConfiguration Configuration { get; }
 
-            services.AddDefaultIdentity<ApplicationUser>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
+//         // This method gets called by the runtime. Use this method to add services to the container.
+//         public void ConfigureServices(IServiceCollection services)
+//         {
+//             services.AddDbContext<ApplicationDbContext>(options =>
+//                 {
+//                     options.EnableSensitiveDataLogging();
+//                     options.UseNpgsql(
+//                         Configuration.GetConnectionString("DefaultConnection"));
+//                 }
+//             );
 
-            services.Configure<IdentityOptions>(options =>
-            {
-                // Password settings.
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireUppercase = false;
+//             services.AddDefaultIdentity<ApplicationUser>()
+//                 .AddEntityFrameworkStores<ApplicationDbContext>()
+//                 .AddDefaultTokenProviders();
 
-                // Lockout settings.
-                options.Lockout.MaxFailedAccessAttempts = 10;
+//             services.Configure<IdentityOptions>(options =>
+//             {
+//                 // Password settings.
+//                 options.Password.RequireNonAlphanumeric = false;
+//                 options.Password.RequireUppercase = false;
 
-                // User settings.
-                options.User.AllowedUserNameCharacters = "aábcdðeéfghiíjklmnoópqrstuvwxyýzþæöAÁBCDÐEÉFGHIÍJKLMNOÓPQRSTUVWXYÝZÞÆÖ0123456789 -._@+";
-                options.User.RequireUniqueEmail = true;
-            });
+//                 // Lockout settings.
+//                 options.Lockout.MaxFailedAccessAttempts = 10;
 
-            services
-                .AddAuthentication(options =>
-                {
-                    // Identity made Cookie authentication the default.
-                    // However, we want JWT Bearer Auth to be the default.
-                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//                 // User settings.
+//                 options.User.AllowedUserNameCharacters = "aábcdðeéfghiíjklmnoópqrstuvwxyýzþæöAÁBCDÐEÉFGHIÍJKLMNOÓPQRSTUVWXYÝZÞÆÖ0123456789 -._@+";
+//                 options.User.RequireUniqueEmail = true;
+//             });
 
-                })
-                .AddJwtBearer(options =>
-                {
-                    options.RequireHttpsMetadata = false;
-                    options.SaveToken = true;
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        ValidIssuer = "https://bitar.is",
-                        ValidAudience = "https://bitar.is",
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetSection("JwtSettings:JwtKey").Value)),
-                    };
-                });
+//             services
+//                 .AddAuthentication(options =>
+//                 {
+//                     // Identity made Cookie authentication the default.
+//                     // However, we want JWT Bearer Auth to be the default.
+//                     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//                     options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+//                     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
 
-            services.Configure<ForwardedHeadersOptions>(options => options.ForwardedHeaders = ForwardedHeaders.All);
-            services.Configure<JwtSettings>(Configuration.GetSection("JwtSettings"));
-            services.Configure<BitcoinSettings>(Configuration.GetSection("BitcoinSettings"));
-            services.Configure<LandsbankinnSettings>(Configuration.GetSection("LandsbankinnSettings"));
-            services.Configure<KrakenSettings>(Configuration.GetSection("KrakenSettings"));
-            services.Configure<JaSettings>(Configuration.GetSection("JaSettings"));
-            services.Configure<DilisenseSettings>(Configuration.GetSection("DilisenseSettings"));
+//                 })
+//                 .AddJwtBearer(options =>
+//                 {
+//                     options.RequireHttpsMetadata = false;
+//                     options.SaveToken = true;
+//                     options.TokenValidationParameters = new TokenValidationParameters
+//                     {
+//                         ValidateIssuerSigningKey = true,
+//                         ValidIssuer = "https://bitar.is",
+//                         ValidAudience = "https://bitar.is",
+//                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetSection("JwtSettings:JwtKey").Value)),
+//                     };
+//                 });
 
-            services.AddHttpClient<ArionService>();
-            services.AddHttpClient<BlockchainService>();
+//             services.Configure<ForwardedHeadersOptions>(options => options.ForwardedHeaders = ForwardedHeaders.All);
+//             services.Configure<JwtSettings>(Configuration.GetSection("JwtSettings"));
+//             services.Configure<BitcoinSettings>(Configuration.GetSection("BitcoinSettings"));
+//             services.Configure<LandsbankinnSettings>(Configuration.GetSection("LandsbankinnSettings"));
+//             services.Configure<KrakenSettings>(Configuration.GetSection("KrakenSettings"));
+//             services.Configure<JaSettings>(Configuration.GetSection("JaSettings"));
+//             services.Configure<DilisenseSettings>(Configuration.GetSection("DilisenseSettings"));
 
-            services.AddScoped<MarketRepository>();
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            services.AddSingleton<BitcoinService>();
-            services.AddSingleton<LandsbankinnService>();
-            services.AddSingleton<AssetService>();
-            services.AddSingleton<KrakenService>();
-            services.AddSingleton<TickerService>();
-            services.AddSingleton<OhlcService>();
-            services.AddHostedService<MarketService>();
+//             services.AddHttpClient<ArionService>();
+//             services.AddHttpClient<BlockchainService>();
 
-            services.AddControllers();
+//             services.AddScoped<MarketRepository>();
+//             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+//             services.AddSingleton<BitcoinService>();
+//             services.AddSingleton<LandsbankinnService>();
+//             services.AddSingleton<AssetService>();
+//             services.AddSingleton<KrakenService>();
+//             services.AddSingleton<TickerService>();
+//             services.AddSingleton<OhlcService>();
+//             services.AddHostedService<MarketService>();
 
-            services.AddSignalR();
+//             services.AddControllers();
 
-            services.AddOpenApiDocument(c =>
-            {
-                c.Title = "Bitar API";
-                c.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("JWT"));
-                c.AddSecurity("JWT", new OpenApiSecurityScheme
-                {
-                    Type = OpenApiSecuritySchemeType.ApiKey,
-                    Name = "Authorization",
-                    In = OpenApiSecurityApiKeyLocation.Header,
-                    Description = "Copy this into the value field: Bearer {token}",
-                });
-                c.PostProcess = document =>
-                {
-                    document.Info.Version = "v1";
-                    document.Info.Title = "Bitar API";
-                    document.Info.Description = "Endilega láttu okkur vita ef það vantar eitthvað eða þér finnst eitthvað mega bæta.";
-                };
-            });
-        }
+//             services.AddSignalR();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+//             // services.AddOpenApiDocument(c =>
+//             // {
+//             //     c.Title = "Bitar API";
+//             //     c.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("JWT"));
+//             //     c.AddSecurity("JWT", new OpenApiSecurityScheme
+//             //     {
+//             //         Type = OpenApiSecuritySchemeType.ApiKey,
+//             //         Name = "Authorization",
+//             //         In = OpenApiSecurityApiKeyLocation.Header,
+//             //         Description = "Copy this into the value field: Bearer {token}",
+//             //     });
+//             //     c.PostProcess = document =>
+//             //     {
+//             //         document.Info.Version = "v1";
+//             //         document.Info.Title = "Bitar API";
+//             //         document.Info.Description = "Endilega láttu okkur vita ef það vantar eitthvað eða þér finnst eitthvað mega bæta.";
+//             //     };
+//             // });
+//         }
 
-            app.UseRouting();
+//         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+//         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//         {
+//             if (env.IsDevelopment())
+//             {
+//                 app.UseDeveloperExceptionPage();
+//             }
 
-            app.UseForwardedHeaders();
+//             app.UseRouting();
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+//             app.UseForwardedHeaders();
 
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
-            app.UseOpenApi(c =>
-            {
-                c.Path = "/{documentName}/swagger.json";
-            });
+//             app.UseAuthentication();
+//             app.UseAuthorization();
 
-            // Enable middleware to serve ReDoc (HTML, JS, CSS, etc.).
-            app.UseReDoc(c =>
-            {
-                c.DocumentTitle = "Bitar API Documentation";
-                c.EnableUntrustedSpec();
-                c.ExpandResponses("200,201");
-                c.HideLoading();
-                c.DisableSearch();
-                c.RoutePrefix = string.Empty;
-            });
+//             // Enable middleware to serve generated Swagger as a JSON endpoint.
+//             // app.UseOpenApi(c =>
+//             // {
+//             //     c.Path = "/{documentName}/swagger.json";
+//             // });
 
-            app.UseCors(builder => builder
-                .WithOrigins("https://bitar.is", "https://www.bitar.is", "http://localhost:4200", "https://innskraning.island.is")
-                .AllowAnyHeader()
-                .AllowAnyMethod()
-                .AllowCredentials());
+//             // Enable middleware to serve ReDoc (HTML, JS, CSS, etc.).
+//             // app.UseReDoc(c =>
+//             // {
+//             //     c.DocumentTitle = "Bitar API Documentation";
+//             //     c.EnableUntrustedSpec();
+//             //     c.ExpandResponses("200,201");
+//             //     c.HideLoading();
+//             //     c.DisableSearch();
+//             //     c.RoutePrefix = string.Empty;
+//             // });
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapHub<TickerHub>("/tickers");
-                endpoints.MapControllers();
-            });
-        }
-    }
-}
+//             app.UseCors(builder => builder
+//                 .WithOrigins("https://bitar.is", "https://www.bitar.is", "http://localhost:4200", "https://innskraning.island.is")
+//                 .AllowAnyHeader()
+//                 .AllowAnyMethod()
+//                 .AllowCredentials());
+
+//             app.UseEndpoints(endpoints =>
+//             {
+//                 endpoints.MapHub<TickerHub>("/tickers");
+//                 endpoints.MapControllers();
+//             });
+//         }
+//     }
+// }
